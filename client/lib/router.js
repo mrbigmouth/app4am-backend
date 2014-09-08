e75a5330230102a3272fc8d4a44f4f55db285437
mrbigmouth <b92101007@gmail.com>
2014-09-08 10:38:40 +0800

    add d3 and analysis base

--- client/lib/router.js
+++ client/lib/router.js
@@ -2,12 +2,29 @@ Router.map(function() {
   //首頁
   this.route(
     "assign"
-  , {"path"   : '/'
+  , {"path"   : "/"
     ,"waitOn" :
         function() {
           return [
             Meteor.subscribe("unAssignNews", new Date())
           ]
+        }
+    }
+  );
+
+  //分析
+  this.route(
+    "analysis"
+  , {"path"   : "/topic/:_id"
+    ,"waitOn" :
+        function() {
+          return [
+            Meteor.subscribe("newsByTopic", this.params._id)
+          ]
+        }
+    ,"data"   :
+        function() {
+          return DB.topic.findOne(new Meteor.Collection.ObjectID(this.params._id))
         }
     }
   );
