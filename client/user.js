2333a662d4dc7e41f367279fd6cda3aa9a0803a8
LeeYong Qian <bob.lee@vpon.com>
2015-02-14 13:13:09 +0800

    樣式調整

--- client/user.js
+++ client/user.js
@@ -1,13 +1,8 @@
-Meteor.loginAs =
-    function(user) {
-      Accounts.callLoginMethod(
-        {"methodArguments" : [ {"method" : "loginAs", "user" : user} ]
-        ,"userCallback"    : function() { console.log(arguments); }
-        }
-      );
-    };
-
-Accounts.ui.config(
-  {"passwordSignupFields" : "USERNAME_AND_OPTIONAL_EMAIL"
-  }
-);
+// Meteor.loginAs =
+//     function(user) {
+//       Accounts.callLoginMethod(
+//         {"methodArguments" : [ {"method" : "loginAs", "user" : user} ]
+//         ,"userCallback"    : function() { console.log(arguments); }
+//         }
+//       );
+//     };
